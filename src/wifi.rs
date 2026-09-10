82872a4c4d2d819985f592454e26ec7ff8953600
Lioquacht-Religion <basik0010@gmail.com>
2026-09-11 00:36:40 +0200

    refactor: store data without connecting to wifi, if light intensity or battery voltage is low

--- src/wifi.rs
+++ src/wifi.rs
@@ -1,57 +1,49 @@
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
     wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
 use log::{error, info};
 
-pub fn wifi(
+pub fn wifi<'m>(
     ssid: &str,
     pass: &str,
-    modem: esp_idf_svc::hal::modem::Modem<'static>,
+    modem: &'m mut esp_idf_svc::hal::modem::Modem,
     sysloop: EspSystemEventLoop,
-) -> Result<(Box<EspWifi<'static>>, Vec<anyhow::Error>), anyhow::Error> {
+) -> Result<(Box<EspWifi<'m>>, Vec<anyhow::Error>), anyhow::Error> {
     let mut auth_method = AuthMethod::WPA2Personal;
     if ssid.is_empty() {
-        return Err(anyhow!("Missing WiFi name"))
+        return Err(anyhow!("Missing WiFi name"));
     }
     if pass.is_empty() {
         auth_method = AuthMethod::None;
         info!("Wifi password is empty");
     }
-    let mut esp_wifi = EspWifi::new(modem, sysloop.clone(), None)
-        .map_err(|e| anyhow!(e))?;
+    let mut esp_wifi = EspWifi::new(&mut *modem, sysloop.clone(), None).map_err(|e| anyhow!(e))?;
     // Connect to the Wi-Fi network
     const WIFI_CONNECT_RETRIES: usize = 10;
     let mut wifi_connect_tries = 0;
     let mut connect_errors: Vec<anyhow::Error> = Vec::new();
-        loop{
-            match try_wifi_connect(
-                ssid,
-                pass,
-                sysloop.clone(),
-                &mut esp_wifi,
-                auth_method,
-            ){
-                Ok(()) => break,
-                Err(e) => {
-                    error!("wifi connection error: {}", e);
-                    if wifi_connect_tries >= WIFI_CONNECT_RETRIES{
-                        return Err(e);
-                    }
-                    connect_errors.push(e);
+    loop {
+        match try_wifi_connect(ssid, pass, sysloop.clone(), &mut esp_wifi, auth_method) {
+            Ok(()) => return Ok((Box::new(esp_wifi), connect_errors)),
+            Err(e) => {
+                error!("wifi connection error: {}", e);
+                if wifi_connect_tries >= WIFI_CONNECT_RETRIES {
+                    return Err(e);
                 }
+                connect_errors.push(e);
             }
-            wifi_connect_tries += 1;
-        };
-    Ok((Box::new(esp_wifi), connect_errors))
+        }
+        wifi_connect_tries += 1;
+    }
 }
 
-pub fn try_wifi_connect(
+pub fn try_wifi_connect<'m>(
     ssid: &str,
     pass: &str,
     sysloop: EspSystemEventLoop,
-    esp_wifi: &mut EspWifi<'static>,
+    esp_wifi: &mut EspWifi<'m>,
     auth_method: AuthMethod,
 ) -> Result<()> {
     let mut wifi = BlockingWifi::wrap(esp_wifi, sysloop)?;
